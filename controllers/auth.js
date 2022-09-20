5c9a5078d8ecc924e1bfa0cc5e028e6883fa1ef3
KararAlsh <kararabdulsahib@hotmail.com>
2022-09-20 19:36:00 +0700

    updated the login post to check if the user exist if so it logs in, if not shows error. Updated the logout post, redirects to login

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,30 +12,62 @@ router.get('/login', (req, res) => {
 router.get('/signup', (req, res) => {
   res.render('signup')
 })
-
-router.post('/login', (req, res) => {})
-
+// to log in
+router.post('/login', async (req, res, next) => {
+  try {
+    let foundUser = await users.findOne({
+      email: req.body.email,
+      password: req.body.password
+    })
+    if (foundUser) {
+      req.login(foundUser, err => {
+        if (err) {
+          throw new Error('Email or password might be wrong, try again')
+        } else {
+          res.redirect('/houses')
+        }
+      })
+    } else {
+      throw new Error('Email or password might be wrong, try again')
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+// end of log in
+// to sign up
 router.post('/signup', async (req, res, next) => {
-  console.log(req.body)
-  let user = await users.create(req.body)
-  console.log('done')
-  req.login(user, err => {
-    try {
-      if (
-        users.findOne({
-          email: req.body.email
-        })
-      ) {
-        throw new Error('User with this email already exists')
-      } else {
-        res.redirect('/houses')
-      }
-    } catch (err) {
+  try {
+    if (
+      await users.findOne({
+        email: req.body.email
+      })
+    ) {
+      throw new Error('User with this email already exists')
+    } else {
+      let user = await users.create(req.body)
+      req.login(user, err => {
+        if (err) throw new Error(err)
+        else res.redirect('/houses')
+      })
+    }
+  } catch (err) {
+    next(err)
+    return
+  }
+})
+// end of signup
+// to logout
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.session.destroy(err => {
+    if (err) {
       next(err)
     }
+    res.clearCookie('connect.sid')
+    res.redirect('login')
   })
 })
-
-router.get('/logout', (req, res) => {})
+// end of logout
 // Export
 module.exports = router
