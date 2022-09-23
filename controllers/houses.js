d7ba974ed52b1550534704a058b362456526adce
KararAlsh <kararabdulsahib@hotmail.com>
2022-09-23 15:25:32 +0700

    added filters to the houses list page

--- controllers/houses.js
+++ controllers/houses.js
@@ -4,14 +4,36 @@ const router = express.Router()
 const Houses = require('../models/houses.js')
 //get controller
 router.get('/', async (req, res) => {
+  // shows profile in the nav if user is logged in
   let loggedUser = req.user
-  let houses = await Houses.find({})
-  console.log(houses)
+
+  // query is what is sent by the filter form
+  let filters = {}
+  let sorting = req.query.sort || '_id'
+
+  if (req.query.location && req.query.location != 'Any location') {
+    filters.location = req.query.location
+  }
+
+  if (req.query.rooms && req.query.rooms != 'Any rooms') {
+    filters.rooms = req.query.rooms
+  }
+
+  if (req.query.price && req.query.price > 0) {
+    filters.price = {
+      $lte: req.query.price
+    }
+  }
+
+  let houses = await Houses.find(filters).sort(sorting)
+
   res.render('houses/list', { loggedUser, houses })
 })
 
 router.get('/create', (req, res) => {
+  // authentification function
   if (req.isAuthenticated()) {
+    // shows profile in the nav if user is logged in
     let loggedUser = req.user
     res.render('./houses/create', { loggedUser })
   } else {
@@ -20,6 +42,7 @@ router.get('/create', (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
+  // this finds the house in the databasa when you click on on it in the list page and renders the page for it, it also populates it with a host
   const house = await Houses.findById(req.params.id).populate('host')
   console.log(house)
   let loggedUser = req.user
@@ -45,6 +68,7 @@ router.post('/', async (req, res, next) => {
 
   // creating house
   let host = req.user._id
+  // this function creates the house for us using the form in th ecreate.hbs then renders the house we've just created page
   const createdHouse = await Houses.create({
     rooms: req.body.rooms,
     price: req.body.price,
