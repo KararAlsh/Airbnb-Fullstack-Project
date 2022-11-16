7e2c9070b9ad929d47b767e71cc2dc66d00a7efe
KararAlsh <kararabdulsahib@hotmail.com>
2022-11-16 09:37:27 +0100

    small modifications

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,17 +1,36 @@
 // Import Packages
 const express = require('express')
 const router = express.Router()
+// models
+const Users = require('../models/users.js')
+const Houses = require('../models/houses.js')
+//get controller
 
-// get controller
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  let loggedUser = req.user
   if (req.isAuthenticated()) {
-    let loggedUser = req.user
-    res.render('./profile', { loggedUser })
+    let houses = await Houses.find({
+      host: req.user._id
+    })
+    res.render('profile', { loggedUser, houses })
   } else {
-    res.redirect('/auth/login')
+    res.redirect('../auth/login')
   }
 })
 
-router.patch('/', (req, res) => {})
+router.patch('/profile', async (req, res) => {
+  let loggedUser = req.user
+  if (req.isAuthenticated()) {
+    // refreshing with new data through the form using findByIdAndUpdate
+    let user = await Users.findByIdAndUpdate(req.user._id, req.body, {
+      new: true
+    })
+    req.login(user, err => {
+      res.redirect('/profile')
+    })
+  } else {
+    res.redirect('../auth/login')
+  }
+})
 // Export
 module.exports = router
