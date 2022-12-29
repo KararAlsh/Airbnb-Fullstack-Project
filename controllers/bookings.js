a47937a6bd54864c540debae298191f8dfb2f289
KararAlsh <kararabdulsahib@hotmail.com>
2022-12-29 11:21:25 +0100

    Updated booking.js so customer is able to book accomidation

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -1,14 +1,27 @@
-// Import Packages
+// Packages
 const express = require('express')
 const router = express.Router()
+// Models
+const Bookings = require('../models/bookings')
 
-// get controller
-router.post('/', (req, res) => {
-  if (req.isAuthenticated()) {
-    res.redirect('/bookings')
-  } else {
-    res.redirect('../auth/login')
+// Post ...
+router.post('/', async (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+      let booking = {
+        author: req.user._id,
+        house: req.body.house,
+        description: req.body.description
+      }
+      await Bookings.create(booking)
+      res.redirect(`/houses/${req.body.house}`)
+    } else {
+      res.redirect('/auth/login')
+    }
+  } catch (err) {
+    res.redirect('/error')
   }
 })
+
 // Export
 module.exports = router
