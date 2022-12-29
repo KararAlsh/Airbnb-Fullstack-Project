456057670fc72c582b2239693319b468f097326a
KararAlsh <kararabdulsahib@hotmail.com>
2022-12-29 12:34:05 +0100

    re wrote code for houses.js and added few comments

--- controllers/houses.js
+++ controllers/houses.js
@@ -1,102 +1,207 @@
-// Import Packages
+// Packages
 const express = require('express')
 const router = express.Router()
-const Houses = require('../models/houses.js')
-//get controller
+// Models
+const Houses = require('../models/houses')
+const Bookings = require('../models/bookings')
+const Reviews = require('../models/reviews')
+
+// Root
 router.get('/', async (req, res) => {
-  // shows profile in the nav if user is logged in
-  let loggedUser = req.user
-
-  // query is what is sent by the filter form
-  let filters = {}
-  let sorting = req.query.sort || '_id'
-
-  if (req.query.location && req.query.location != 'Any location') {
-    filters.location = req.query.location
-  }
-
-  if (req.query.rooms && req.query.rooms != 'Any rooms') {
-    filters.rooms = req.query.rooms
-  }
-
-  if (req.query.price && req.query.price > 0) {
-    filters.price = {
-      $lte: req.query.price
+  try {
+    // if query set filter object else blank object
+    let query = req.query.filtering
+      ? {
+          location: req.query.location,
+          rooms: req.query.rooms,
+          price:
+            req.query.maxPrice == ''
+              ? { $gte: 0 }
+              : { $lte: req.query.maxPrice },
+          title: { $regex: req.query.title, $options: 'i' }
+        }
+      : {}
+    // delete 'any' from folder filter
+    if (query.location == 'any') delete query.location
+    if (query.rooms == 'any') delete query.rooms
+    // sort results
+    let houses = await Houses.find(query).sort(
+      req.query.sort ? req.query.sort : 'price'
+    )
+    // render with different navbars
+    if (req.isAuthenticated()) {
+      res.render('houses/list', {
+        // !!! change to just user & update templates
+        user: { name: req.user.name, avatar: req.user.avatar },
+        auth: req.isAuthenticated(),
+        houses
+      })
+    } else {
+      res.render('houses/list', { houses })
     }
-  }
-
-  let houses = await Houses.find(filters).sort(sorting)
-
-  res.render('houses/list', { loggedUser, houses })
-})
-
-router.get('/create', (req, res) => {
-  // authentification function
-  if (req.isAuthenticated()) {
-    // shows profile in the nav if user is logged in
-    let loggedUser = req.user
-    res.render('./houses/create', { loggedUser })
-  } else {
-    res.redirect('/auth/login')
+  } catch (err) {
+    // !!! change for next function and throw errors
+    res.redirect('/error')
   }
 })
 
+// Get create
+router.get('/create', async (req, res) => {
+  try {
+    let photosX = []
+    for (i = 0; i < 9; i++) {
+      photosX.push('')
+    }
+    if (req.isAuthenticated()) {
+      res.render('../views/houses/create', {
+        user: { name: req.user.name, avatar: req.user.avatar },
+        auth: req.isAuthenticated(),
+        hostID: req.user._id,
+        photosX
+      })
+    } else {
+      res.redirect('/auth/login')
+    }
+  } catch (err) {
+    res.redirect('/error')
+  }
+})
+
+// Get :id
 router.get('/:id', async (req, res) => {
-  // this finds the house in the databasa when you click on on it in the list page and renders the page for it, it also populates it with a host
-  const house = await Houses.findById(req.params.id).populate('host')
-  console.log(house)
-  let loggedUser = req.user
+  try {
+    let house = await Houses.findById(req.params.id).populate('host')
+    // Render the houses/one template, passing the house object
+    // !!! make this work even when you're not signed in
+    if (req.isAuthenticated()) {
+      // house form template needs house and user id to determine form block
+      // !!! why the two lines below?
+      let bookingExists = (await Bookings.findOne({
+        author: req.user._id,
+        house: req.params.id
+      }))
+        ? true
+        : false
+      //find all the reviews in the database, that belong to the house in question
+      // !!! only pull through avatar and name - leave pw and email out
+      let reviews = await Reviews.find({ house: req.params.id }).populate(
+        'author'
+      )
+      // above should pull rating now for that house
+      // function to sum all ratings
+      let score = 0
+      reviews.forEach(review => {
+        score += review.rating
+      })
 
-  res.render('houses/one', { loggedUser, house })
-})
-
-router.get('/:id/edit', (req, res) => {
-  if (req.isAuthenticated()) {
-    let loggedUser = req.user
-    res.render('./houses/edit', { loggedUser })
-  } else {
-    res.redirect('/auth/login')
+      res.render('../views/houses/one', {
+        user: { name: req.user.name, avatar: req.user.avatar },
+        auth: req.isAuthenticated(),
+        house, // !!! - can I do await in-line here?
+        hostName: house.host.name,
+        hostAvatar: house.host.avatar,
+        bookingExists,
+        reviews,
+        score,
+        reviewCount: reviews.length
+      })
+    } else {
+      res.render('../views/houses/one', {
+        // !!! Populate its host field with host object - then pull user.name - ???
+        // !!! test this - how it looks not logged in (same logic elsewhere)
+        house, // !!! - can I do await in-line here?
+        hostName: housePop.host.name,
+        hostAvatar: housePop.host.avatar
+      })
+    }
+  } catch (err) {
+    res.redirect('/error')
   }
 })
 
-router.post('/', async (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    res.redirect('/auth/login')
-  }
-
-  console.log(req.body)
-
-  // creating house
-  let host = req.user._id
-  // this function creates the house for us using the form in th ecreate.hbs then renders the house we've just created page
-  const createdHouse = await Houses.create({
-    rooms: req.body.rooms,
-    price: req.body.price,
-    location: req.body.location,
-    description: req.body.description,
-    title: req.body.title,
-    photos: req.body.photos,
-    host: host
-  })
-  res.redirect(`/houses/${createdHouse._id}`)
-})
-
-router.patch('/:id', (req, res) => {
-  if (req.isAuthenticated()) {
-    let loggedUser = req.user
-    res.render('./houses', { loggedUser })
-  } else {
-    res.redirect('/auth/login')
+// Get :id/edit
+router.get('/:id/edit', async (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+      let house = await Houses.findById(req.query.listingID)
+      res.render('../views/houses/edit', {
+        user: { name: req.user.name, avatar: req.user.avatar },
+        auth: req.isAuthenticated(),
+        house
+      })
+    } else {
+      res.redirect('/auth/login')
+    }
+  } catch (err) {
+    res.redirect('/error')
   }
 })
 
-router.delete('/:id', (req, res) => {
-  if (req.isAuthenticated()) {
-    let loggedUser = req.user
-    res.render('./houses', { loggedUser })
-  } else {
-    res.redirect('/auth/login')
+// Post root
+router.post('/', async (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+      req.body.host = req.user._id
+      // !!! remove nested catch
+      try {
+        await Houses.create(req.body)
+        // go to the house page of the newly created house
+        let house = await Houses.findOne(req.body)
+        res.redirect(`houses/${house._id}`)
+      } catch (err) {
+        console.log('error - !!! do these properly')
+      }
+    } else {
+      res.redirect('/auth/login')
+    }
+  } catch (err) {
+    res.redirect('/error')
   }
 })
+
+// Patch :id
+router.patch('/:id', async (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+      // extract command, clean object for find
+      command = req.body.command
+      delete req.body.command
+      if (command == 'update') {
+        // console.log('update')
+        // !!! doesn't update photos
+        await Houses.findByIdAndUpdate(req.params.id, req.body)
+        res.redirect(`/houses/${req.params.id}`)
+      } else {
+        // delete house from db
+        console.log('delete')
+        await Houses.findByIdAndDelete(req.params.id)
+        console.log('deleted')
+        // redirect to profile
+        res.redirect('/profile')
+        console.log('house deleted')
+        // !!! backburner: are you sure?
+      }
+    } else {
+      console.log('not logged in')
+      res.redirect('/auth/login')
+    }
+  } catch (err) {
+    res.redirect('/error')
+  }
+})
+
+// Delete :id
+router.delete('/:id', async (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+			console.log('does not do anything')
+    } else {
+      res.redirect('/auth/login')
+    }
+  } catch (err) {
+    res.redirect('/error')
+  }
+})
+
 // Export
 module.exports = router
